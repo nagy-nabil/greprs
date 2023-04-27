6e95b81597ac5323f2d656495738b8e58ec96520
nagy nabil <nagynabil651@gmail.com>
2023-04-27 17:09:19 +0200

    search

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,38 @@ impl Config {
     }
 }
 
+// Box<dyn Error> to box different kinds of errors and know their underlying type at runtime
+// Box do operation for us => convert any type that impl Error trait to Box<Error>
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("{}", content);
-    Ok(())
+    for line in search(&config.query, &content) {
+        println!("🪵 [lib.rs:27]~ token ~ \x1b[0;32mline\x1b[0m = {}", line);
+    }
+    return Ok(());
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&str> = Vec::new();
+    for line in content.split("\n") {
+        if line.contains(query) {
+            res.push(line);
+        }
+    }
+    return res;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
